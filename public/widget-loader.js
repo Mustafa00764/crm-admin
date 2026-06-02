ae0928bd93c174e8efdeb89228a13618fc1bf6c2
Mustafa00764 <eshchanov9@gmail.com>
2026-06-02 17:24:10 +0500

    edit chat style

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -88,7 +88,7 @@
 
     container.style.width = openedWidth + 'px'
     container.style.height = openedHeight + 'px'
-    container.style.borderRadius = '17.5px'
+    container.style.borderRadius = '18.5px'
     applyPosition()
   }
 
