8aea0501485d3124c4e2e6005c7e88fc40c5d77a
Mustafa00764 <eshchanov9@gmail.com>
2026-06-04 21:12:40 +0500

    edit ai chat

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -8,7 +8,7 @@
   var position = currentScript.getAttribute('data-position') || 'right'
   var theme = currentScript.getAttribute('data-theme') || 'dark'
 
-  // var closedSize = 56
+  var closedSize = 56
   var openedWidth = 380
   var openedHeight = 620
   var offset = 20
