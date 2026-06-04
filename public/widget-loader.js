9e08977d8897ce90609dfcbbc5c1df7b1122bd54
Mustafa00764 <eshchanov9@gmail.com>
2026-06-04 22:14:19 +0500

    edit widget-loader.js

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -216,7 +216,7 @@
   Object.assign(tooltip.style, {
     position: 'absolute',
     left: buttonSize + 12 + 'px',
-    bottom: '14px',
+    bottom: '50%',
     maxWidth: '300px',
     padding: '10px 14px',
     borderRadius: '14px',
@@ -230,7 +230,7 @@
     boxShadow: '0 12px 30px rgba(0, 0, 0, .16)',
     opacity: '0',
     visibility: 'hidden',
-    transform: 'translateX(-6px)',
+    transform: 'translateX(-6px) translateY(50%)',
     pointerEvents: 'none',
   })
 
