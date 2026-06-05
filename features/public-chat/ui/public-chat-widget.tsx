3550a91476744dbed40173b3f97cdf9f35e66dce
Mustafa00764 <eshchanov9@gmail.com>
2026-06-05 09:44:51 +0500

    edit chat messege

--- features/public-chat/ui/public-chat-widget.tsx
+++ features/public-chat/ui/public-chat-widget.tsx
@@ -271,7 +271,55 @@ export function PublicChatWidget({
         ))}
 
         {pending ? (
-          <div className="text-xs opacity-60">Анна печатает...</div>
+          <div
+            className={cn(
+              'text-xs w-[50%] flex items-end',
+              theme === 'light'
+                ? ' text-slate-900/60'
+                : ' text-white/60'
+            )}
+          >
+            <p>Анна печатает</p>
+            <svg
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-2.5 ml-0.5"
+            >
+              <circle cx="4" cy="12" r="3">
+                <animate
+                  id="spinner_qFRN"
+                  begin="0;spinner_OcgL.end+0.25s"
+                  attributeName="cy"
+                  calcMode="spline"
+                  dur="0.6s"
+                  values="12;6;12"
+                  keySplines=".33,.66,.66,1;.33,0,.66,.33"
+                />
+              </circle>
+              <circle cx="12" cy="12" r="3">
+                <animate
+                  begin="spinner_qFRN.begin+0.1s"
+                  attributeName="cy"
+                  calcMode="spline"
+                  dur="0.6s"
+                  values="12;6;12"
+                  keySplines=".33,.66,.66,1;.33,0,.66,.33"
+                />
+              </circle>
+              <circle cx="20" cy="12" r="3">
+                <animate
+                  id="spinner_OcgL"
+                  begin="spinner_qFRN.begin+0.2s"
+                  attributeName="cy"
+                  calcMode="spline"
+                  dur="0.6s"
+                  values="12;6;12"
+                  keySplines=".33,.66,.66,1;.33,0,.66,.33"
+                />
+              </circle>
+            </svg>
+          </div>
         ) : null}
       </div>
 
