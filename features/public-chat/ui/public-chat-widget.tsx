e4852c60aaddf7b64996f2b8a9a7cd85871de0fc
Mustafa00764 <eshchanov9@gmail.com>
2026-06-09 12:59:08 +0500

    edit chat icon

--- features/public-chat/ui/public-chat-widget.tsx
+++ features/public-chat/ui/public-chat-widget.tsx
@@ -1359,7 +1359,7 @@ export function PublicChatWidget({
         )}
       >
         <div className="flex items-center gap-3">
-          <div className=" w-10 h-10 bg-[#08b7ef]/50 text-slate-900">
+          <div className="flex items-center justify-center rounded-full w-10 h-10 bg-[#08b7ef] text-white">
             <Mic className="h-6 w-6" />
           </div>
 
