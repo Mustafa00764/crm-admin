f7bdc0ea9f9a7159ce018fe959d35b0529885e0c
Mustafa00764 <eshchanov9@gmail.com>
2026-06-12 15:44:37 +0500

    isLogin

--- widgets/admin-shell/ui/admin-shell.tsx
+++ widgets/admin-shell/ui/admin-shell.tsx
@@ -1,25 +1,25 @@
-'use client'
+// 'use client'
 
 import { PublicChatWidget } from '@/features/public-chat/ui/public-chat-widget'
 import { AdminNavMenu } from './admin-nav-menu'
 import { AdminSidebar } from './admin-sidebar'
-import { useEffect } from 'react'
-import { notFound } from 'next/navigation'
+// import { useEffect } from 'react'
+// import { notFound } from 'next/navigation'
 
 type AdminShellProps = {
   children: React.ReactNode
 }
 
 export function AdminShell({ children }: AdminShellProps) {
-  const isLogin = localStorage.getItem('login') === 'F4@xN8!wP3#sY7qD' || false
+  // const isLogin = localStorage.getItem('login') === 'F4@xN8!wP3#sY7qD' || false
 
-  useEffect(() => {
-    localStorage.setItem('login', '')
-  }, [])
+  // useEffect(() => {
+  //   localStorage.setItem('login', '')
+  // }, [])
 
-  if (!isLogin) {
-    notFound()
-  }
+  // if (!isLogin) {
+  //   notFound()
+  // }
 
   return (
     <div className="crm-page min-h-screen h-auto pb-24 lg:pb-0">
